af77cdaed665b927798d50daafba5dc0e0777f81
EMendibil <83004847+EMendibil@users.noreply.github.com>
2023-09-13 17:17:54 +0200

    Instalatzaileak igota

---
+++ Jolasmatika/JolasMAKER-Installers/1 Cygwin-Installer.bat
@@ -0,0 +1,1 @@
+start /B /WAIT setup-x86_64.exe --site https://mirrors.aliyun.com/cygwin/ --root C:\cygwin64 --quiet-mode --no-shortcuts --packages subversion,python3,make,wget,unzip
---
+++ Jolasmatika/JolasMAKER-Installers/JolasMAKER-Installer-Linux.bat
@@ -0,0 +1,13 @@
+sudo apt install subversion
+sudo apt install wget
+sudo apt install default-jre
+sudo apt install python3
+sudo apt install sed
+
+wget https://github.com/EMendibil/blockly-games-iruzkinak/archive/master.zip
+unzip master.zip
+rm master.zip
+rm /home/$USER/Documents/blockly-games-iruzkinak
+mv -f blockly-games-iruzkinak-master /home/$USER/Documents/blockly-games-iruzkinak
+cd /home/$USER/Documents/blockly-games-iruzkinak
+make deps
